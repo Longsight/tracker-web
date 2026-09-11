9b727a2af3d8c41a2c395f1f80cdc00e39a9c53b
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-12 00:34:53 +0100

    add distance title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,12 @@ const competitorStatus = (status) => {
   }
 }
 
+const checkpointTitle = (checkpoint) => {
+  const cumulative = (checkpoint.cumulative / 1000).toFixed(2);
+  const distance = (checkpoint.distance / 1000).toFixed(2);
+  return `From start: ${cumulative}   From last: ${distance}`;
+}
+
 function App() {
   const [fetchedAll, setFetchedAll] = useState(false);
   const [fetchedCompetitor, setFetchedCompetitor] = useState(false);
@@ -221,7 +227,7 @@ function App() {
                     }
                     return (
                       <tr key={index}>
-                        <td>{checkpoint.name}</td>
+                        <td title={checkpointTitle(checkpoint)}>{checkpoint.name}</td>
                         <td>{timing}</td>
                       </tr>
                     );

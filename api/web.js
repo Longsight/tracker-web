9d48987539e7551dd868fabed75e6ca08071a6a4
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-14 16:17:05 +0100

    fix GPX

--- api/web.js
+++ api/web.js
@@ -22,12 +22,10 @@ export const web = (port, db) => {
 
       if (command == 'fetchAll') {
         results = db.prepare(`
-          SELECT c.name, c.bib, c.status, max(t.timestamp) AS timestamp, t.lat, t.lon FROM
-          tracks AS t, races AS r, competitors as c WHERE
-          t.competitor = c.competitorid AND
+          SELECT c.name, c.bib, c.status FROM
+          competitors as c, races AS r WHERE
           c.race = r.raceid AND
           r.tag = @race
-          GROUP BY competitor
         `).all({ race });
       }
 

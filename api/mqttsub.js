7ef15c1966f9c731a990760c08c64da5208a7a1f
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-15 00:43:10 +0100

    fetch and store prefs

--- api/mqttsub.js
+++ api/mqttsub.js
@@ -10,7 +10,7 @@ const ping = (db, msg) => {
   }
 
   const {mac, time, bat, temp, lat, lon, imported} = Object.fromEntries(msg.split(',').map(part => part.split(':')));
-  if (parseInt(lat) == parseInt(lon) == 0) {
+  if (parseInt(lat) == 0 && parseInt(lon) == 0) {
     return;
   }
   const comp = db.prepare(`

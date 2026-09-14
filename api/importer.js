e354f452f0e8689a60e18e98c3f051c0afe1fc2a
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-14 14:02:46 +0100

    add importer

--- api/importer.js
+++ api/importer.js
@@ -47,7 +47,7 @@ export const importer = (files) => {
     if (gpxerr) {
       console.error(gpxerr);
     }
-    log(parsedFile.tracks[0].points[0]);
+    log(JSON.stringify(parsedFile.tracks[0]));
   });
 }
 

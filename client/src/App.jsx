41065254921e564f40d73e615091455615e29a2a
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-14 16:53:53 +0100

    position fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -91,6 +91,7 @@ function App() {
     }
     const { command, results } = lastJsonMessage;
     if (command == 'fetchAll') {
+      console.log(results);
       setCompetitors(results);
       setFocused(results[0].bib);
     }

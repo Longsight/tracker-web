d004c438c4287e43a57aed285561ab9c1f151895
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-14 19:56:40 +0100

    oops

--- api/routes/tracker.js
+++ api/routes/tracker.js
@@ -5,7 +5,7 @@ export const trackerRoute = (db) => {
         SELECT r.sleep_time, r.queue_size, r.start_time, r.finish_time, t.battery_capacity
         FROM trackers AS t, competitors AS c, races as r
         WHERE t.competitor = c.competitorid
-        AND c.race = race.raceid
+        AND c.race = r.raceid
         AND t.mac = @mac
       `).get(req.params);
       res.json(tracker);

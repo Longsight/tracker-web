03ace60ea7fb6adeeeb8f40ebfc6e02b8324275b
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-14 19:47:28 +0100

    return tracker config

--- api/web.js
+++ api/web.js
@@ -15,7 +15,7 @@ export const web = (port, db) => {
   app.set('port', port);
   
   app.ws(`/ws`, wsRoute(db));
-  app.get('/tracker/:mac', trackerRoute(db));
+  app.get('/config/:mac', trackerRoute(db));
   app.use(errorRoute);
   
   server.listen(port);

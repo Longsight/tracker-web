d6b9e65fca1ea5bd4593b66c92d01960bf4dbaaa
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-15 20:08:47 +0100

    better checkpoint interpolation

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -86,7 +86,7 @@ const processGPX = (files, raceIndex) => {
       `);
       var pointIndex = 0;
       var lastMinPoint = 0;
-      var lastMinDist = 100;
+      var lastMinDist = 200;
       var lastCPDist = 0;
       const points = parsedFile.tracks[0].points;
       parsedFile.waypoints.forEach((checkpoint, index) => {
@@ -100,13 +100,13 @@ const processGPX = (files, raceIndex) => {
             lastMinDist = dist;
             break;
           }
-          if (dist < 100) {
+          if (dist < 200) {
             if (dist < lastMinDist) {
               lastMinPoint = pointIndex;
               lastMinDist = dist;
             }
           } else {
-            if (lastMinDist < 100) {
+            if (lastMinDist < 200) {
               break;
             }
           }
@@ -144,7 +144,7 @@ const processGPX = (files, raceIndex) => {
         } catch (e) {
           err(e.message);
         }
-        lastMinDist = 100;
+        lastMinDist = 200;
         lastCPDist = cumulative;
       });
       resolve();

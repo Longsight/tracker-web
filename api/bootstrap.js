4448bca80f689f6c0a7813e611cbee332f498d69
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-15 19:47:14 +0100

    draw checkpoints

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -86,7 +86,7 @@ const processGPX = (files, raceIndex) => {
       `);
       var pointIndex = 0;
       var lastMinPoint = 0;
-      var lastMinDist = 200;
+      var lastMinDist = 50;
       var lastCPDist = 0;
       const points = parsedFile.tracks[0].points;
       parsedFile.waypoints.forEach((checkpoint, index) => {
@@ -100,13 +100,13 @@ const processGPX = (files, raceIndex) => {
             lastMinDist = dist;
             break;
           }
-          if (dist < 200) {
+          if (dist < 50) {
             if (dist < lastMinDist) {
               lastMinPoint = pointIndex;
               lastMinDist = dist;
             }
           } else {
-            if (lastMinDist < 200) {
+            if (lastMinDist < 50) {
               break;
             }
           }

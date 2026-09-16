a2ac2e77fd37f2c4abf218cccc1dcd1ca06ea977
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-16 12:23:26 +0100

    fewer checkpoint points

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -119,7 +119,7 @@ const processGPX = (files, raceIndex) => {
           if (index == 0) {
             return [next];
           } else {
-            const lerpCount = parseInt(haversine(orig[index - 1], next, {unit: 'meter'}), 20);
+            const lerpCount = parseInt(haversine(orig[index - 1], next, {unit: 'meter'}), 50);
             const lerpPoints = [];
             for (var i = 1; i < lerpCount; i++) {
               lerpPoints.push({

441bab0770b90482fe66969e58878481565f96ce
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-16 12:32:11 +0100

    fewer checkpoint points

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -86,7 +86,7 @@ const processGPX = (files, raceIndex) => {
       `);
       var pointIndex = 0;
       var lastMinPoint = 0;
-      var lastMinDist = 200;
+      var lastMinDist = 100;
       var lastCPDist = 0;
       const points = parsedFile.tracks[0].points;
       parsedFile.waypoints.forEach((checkpoint, index) => {
@@ -100,13 +100,13 @@ const processGPX = (files, raceIndex) => {
             lastMinDist = dist;
             break;
           }
-          if (dist < 200) {
+          if (dist < 100) {
             if (dist < lastMinDist) {
               lastMinPoint = pointIndex;
               lastMinDist = dist;
             }
           } else {
-            if (lastMinDist < 200) {
+            if (lastMinDist < 100) {
               break;
             }
           }
@@ -114,7 +114,7 @@ const processGPX = (files, raceIndex) => {
         }
         const coords = JSON.stringify(points.slice(
           lastMinPoint,
-          Math.min(Math.max(lastMinPoint + 2, pointIndex), points.length)
+          Math.min(Math.max(lastMinPoint + 4, pointIndex), points.length)
         ).map(({latitude, longitude}) => ({latitude, longitude})).reduce((memo, next, index, orig) => {
           if (index == 0) {
             return [next];
@@ -144,7 +144,7 @@ const processGPX = (files, raceIndex) => {
         } catch (e) {
           err(e.message);
         }
-        lastMinDist = 200;
+        lastMinDist = 100;
         lastCPDist = cumulative;
       });
       resolve();

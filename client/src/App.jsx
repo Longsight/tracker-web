3d013dd4e1e3756d72af7b9fbf943d346da87e7c
Ashleigh Crosby <tcrosby@gmail.com>
2026-09-16 12:45:41 +0100

    fetch race details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 import { parseGPX } from '@we-gold/gpxjs'
 import haversine from 'haversine';
 
-const DEBUG = false;
+const DEBUG = true;
 
 const initial = [53.284784, -1.089135];
 const raceName = window.location.pathname.replace('/tracker/', '');
